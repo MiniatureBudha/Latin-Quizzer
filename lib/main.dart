13c546b7f3e123e2aa603faa42eb6e138fe3185b
SirKatto <Owenjwarner@gmail.com>
2023-05-31 22:13:05 -0400

    Login Page P1: Started, not finished yet Implementing requirement of username and password to get in, only username active so far Don't know how to make Password input pop up after username success yet Still unsure what the end of Home is

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,77 @@
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app/Pages/Home.dart';
 import 'package:flutter_app/Constants/color_constants.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
+    const appTitle = 'Login Page';
+
     return MaterialApp(
-      home: Home(),
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const LoginInput(),
+      ),
+    );
+  }
+}
+
+class LoginInput extends StatefulWidget {
+  const LoginInput({super.key});
+
+  @override
+  LoginInputState createState() {
+    return LoginInputState();
+  }
+}
+
+class LoginInputState extends State<LoginInput> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Username',
+            ),
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please Enter Username';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  if(equal(_formKey.currentState, 'Bob')){
+                    return Home();
+                  }
+                }
+              },
+              child: const Text('Enter'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
